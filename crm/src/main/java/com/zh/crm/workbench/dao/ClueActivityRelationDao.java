c4dce59b1909336dad97fd2568f8dc27b7ef0131
zhdyx999 <1013300945@qq.com>
2020-12-08 17:19:37 +0800

    12.08提交

---
+++ crm/src/main/java/com/zh/crm/workbench/dao/ClueActivityRelationDao.java
@@ -0,0 +1,9 @@
+package com.zh.crm.workbench.dao;
+
+import com.zh.crm.workbench.domain.ClueActivityRelation;
+
+public interface ClueActivityRelationDao {
+
+	
+
+}
---
+++ crm/src/main/java/com/zh/crm/workbench/dao/ClueDao.java
@@ -0,0 +1,9 @@
+package com.zh.crm.workbench.dao;
+
+import com.zh.crm.workbench.domain.Clue;
+
+public interface ClueDao {
+
+	
+
+}
